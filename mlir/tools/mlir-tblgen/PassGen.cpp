2b7a7eac591525bc7520ee29347fe8470256313e
Alexandru Kigyosi <alexandru.kigyosi@intel.com>
2023-08-03 17:15:33 +0300

    added destructor for generated classes

--- mlir/tools/mlir-tblgen/PassGen.cpp
+++ mlir/tools/mlir-tblgen/PassGen.cpp
@@ -53,6 +53,7 @@ public:
     {1}::operator=(other);
     return *this;
   }
+  ~{0}Base() = default;
 
   /// Returns the command-line argument attached to this pass.
   static constexpr ::llvm::StringLiteral getArgumentName() {
