c87cc9b9acb654f9a3497bccef413ca4e3af9849
Nikita Kudriavtsev <nikita.kudriavtsev@intel.com>
2024-01-31 21:56:33 +0000

    Add method getInlineBlockAndPoint for DialectInlinerInterface

--- mlir/lib/Transforms/Inliner.cpp
+++ mlir/lib/Transforms/Inliner.cpp
@@ -581,7 +581,8 @@ static LogicalResult inlineCallsInSCC(Inliner &inliner, CGUseList &useList,
     useList.mergeUsesAfterInlining(it.targetNode, it.sourceNode);
 
     // then erase the call.
-    call.erase();
+    const auto *callInterface = inliner.getInterfaceFor(call->getDialect());
+    callInterface->eraseCall(call);
 
     // If we inlined in place, mark the node for deletion.
     if (inlineInPlace) {
