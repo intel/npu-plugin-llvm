3a3e6ea7a041031734432699c54a96bec0d902d6
Luca Sarti <luca.sarti@intel.com>
2024-08-20 09:28:02 +0100

    Extend Quant dialect with Quantile Quantization type (#53)
    * Expanding Quant dialect with Quantile Quantized type
    
    * Adding quantile mlir tests
    
    * Adding check on quantiles array size and updated mlir tests

--- mlir/lib/Dialect/Quant/IR/QuantOps.cpp
+++ mlir/lib/Dialect/Quant/IR/QuantOps.cpp
@@ -93,7 +93,8 @@ LogicalResult verifyQuantizationOp(Operation *op, QuantizedType quantizedType,
 
 void QuantDialect::initialize() {
   addTypes<AnyQuantizedType, CalibratedQuantizedType, UniformQuantizedType,
-           UniformQuantizedPerAxisType>();
+           UniformQuantizedPerAxisType, QuantileQuantizedType,
+           QuantileQuantizedPerAxisType>();
   addOperations<
 #define GET_OP_LIST
 #include "mlir/Dialect/Quant/IR/QuantOps.cpp.inc"
