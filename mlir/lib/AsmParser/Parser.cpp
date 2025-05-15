plaidml/llvm-project

[mlir][RFC] Allow to skip operands type specification in ASM form
Based on the following discussion:
https://llvm.discourse.group/t/declarative-assembly-format-requirement-for-type-presence/4399

Relax checks in `OperationParser` - it allows to skip value type specification,
if the value was already defined in the same block.

Differential Revision: https://reviews.llvm.org/D111650

mlir/lib/Parser/Parser.cpp
@@ -700,16 +700,35 @@ Value OperationParser::resolveSSAUse(SSAUseInfo useInfo, Type type) {
   // If we have already seen a value of this name, return it.
   if (useInfo.number < entries.size() && entries[useInfo.number].value) {
     Value result = entries[useInfo.number].value;
-    // Check that the type matches the other uses.
-    if (result.getType() == type)
-      return maybeRecordUse(result);
 
-    emitError(useInfo.loc, "use of value '")
-        .append(useInfo.name,
-                "' expects different type than prior uses: ", type, " vs ",
-                result.getType())
-        .attachNote(getEncodedSourceLocation(entries[useInfo.number].loc))
-        .append("prior use here");
+    // Check that the type matches the other uses.
+    if (type && result.getType() != type) {
+      emitError(useInfo.loc, "use of value '")
+          .append(useInfo.name,
+                  "' expects different type than prior uses: ", type, " vs ",
+                  result.getType())
+          .attachNote(getEncodedSourceLocation(entries[useInfo.number].loc))
+          .append("prior use here");
+      return nullptr;
+    }
+
+    // Explicit type specification can be omitted, only if the Value was already
+    // defined prior in the same block.
+    if (!type && result.getParentBlock() != opBuilder.getBlock()) {
+      emitError(useInfo.loc, "value '")
+          .append(useInfo.name, "' was defined in separate block and requires "
+                                "explicit type definition")
+          .attachNote(getEncodedSourceLocation(entries[useInfo.number].loc))
+          .append("defined here");
+      return nullptr;
+    }
+
+    return maybeRecordUse(result);
+  }
+
+  if (!type) {
+    emitError(useInfo.loc, "forward reference of value '")
+        .append(useInfo.name, "' requires explicit type specification");
     return nullptr;
   }
 

intel/npu-plugin-llvm

mlir/lib/AsmParser/Parser.cpp
@@ -1065,16 +1065,35 @@ Value OperationParser::resolveSSAUse(UnresolvedOperand useInfo, Type type) {
   // If we have already seen a value of this name, return it.
   if (useInfo.number < entries.size() && entries[useInfo.number].value) {
     Value result = entries[useInfo.number].value;
-    // Check that the type matches the other uses.
-    if (result.getType() == type)
-      return maybeRecordUse(result);
 
-    emitError(useInfo.location, "use of value '")
-        .append(useInfo.name,
-                "' expects different type than prior uses: ", type, " vs ",
-                result.getType())
-        .attachNote(getEncodedSourceLocation(entries[useInfo.number].loc))
-        .append("prior use here");
+    // Check that the type matches the other uses.
+    if (type && result.getType() != type) {
+      emitError(useInfo.location, "use of value '")
+          .append(useInfo.name,
+                  "' expects different type than prior uses: ", type, " vs ",
+                  result.getType())
+          .attachNote(getEncodedSourceLocation(entries[useInfo.number].loc))
+          .append("prior use here");
+      return nullptr;
+    }
+
+    // Explicit type specification can be omitted, only if the Value was already
+    // defined prior in the same block.
+    if (!type && result.getParentBlock() != opBuilder.getBlock()) {
+      emitError(useInfo.location, "value '")
+          .append(useInfo.name, "' was defined in separate block and requires "
+                                "explicit type definition")
+          .attachNote(getEncodedSourceLocation(entries[useInfo.number].loc))
+          .append("defined here");
+      return nullptr;
+    }
+
+    return maybeRecordUse(result);
+  }
+
+  if (!type) {
+    emitError(useInfo.location, "forward reference of value '")
+        .append(useInfo.name, "' requires explicit type specification");
     return nullptr;
   }
 