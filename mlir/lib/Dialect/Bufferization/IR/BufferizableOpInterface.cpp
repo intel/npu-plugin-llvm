8fd93cdc7b17c7bd06ffdf335fd16e37e58bc752
Andrei Golubev <andrey.golubev@intel.com>
2025-11-18 10:18:53 +0000

    [mlir][bufferization] Refine tensor-buffer compatibility checks (#167705)
    Generally, to_tensor and to_buffer already perform sufficient
    verification. However, there are some unnecessarily strict constraints:
    * builtin tensor requires its buffer counterpart to always be memref
    * to_buffer on ranked tensor requires to always return memref
    
    These checks are assertions (i.e. preconditions), however, they actually
    prevent an apparently useful bufferization where builtin tensors could
    become custom buffers. Lift these assertions, maintaining the
    verification procedure unchanged, to allow builtin -> custom
    bufferizations at operation boundary level.

--- mlir/lib/Dialect/Bufferization/IR/BufferizableOpInterface.cpp
+++ mlir/lib/Dialect/Bufferization/IR/BufferizableOpInterface.cpp
@@ -656,16 +656,6 @@ bool AnalysisState::hasUndefinedContents(OpOperand *opOperand) const {
   return false;
 }
 
-// bufferization.to_buffer is not allowed to change the rank.
-static void ensureToBufferOpIsValid(Value tensor, Type memrefType) {
-#ifndef NDEBUG
-  auto rankedTensorType = llvm::dyn_cast<RankedTensorType>(tensor.getType());
-  assert((!rankedTensorType || llvm::cast<MemRefType>(memrefType).getRank() ==
-                                   rankedTensorType.getRank()) &&
-         "to_buffer would be invalid: mismatching ranks");
-#endif
-}
-
 FailureOr<Value> bufferization::getBuffer(RewriterBase &rewriter, Value value,
                                           const BufferizationOptions &options) {
 #ifndef NDEBUG
@@ -683,7 +673,7 @@ FailureOr<Value> bufferization::getBuffer(RewriterBase &rewriter, Value value,
   FailureOr<BufferLikeType> bufferType = getBufferType(value, options);
   if (failed(bufferType))
     return failure();
-  ensureToBufferOpIsValid(value, *bufferType);
+
   return rewriter
       .create<bufferization::ToBufferOp>(value.getLoc(), *bufferType, value)
       .getResult();
